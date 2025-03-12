8c5764c41de7df3487c01b3801dfbb94bb724193
Purushotham Guda <purushotham.guda49@gmail.com>
2025-03-12 14:44:50 +0530

    updated dashboard comonent

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
-export default function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
-  );
-}
+import React from "react";
+
+const Dashboard = () => {
+  return <div>Dashboard</div>;
+};
+
+export default Dashboard;
