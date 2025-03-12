da8378d88052cc6a720298fb89e998852e3f298f
Purushotham Guda <purushotham.guda49@gmail.com>
2025-03-12 15:01:08 +0530

    added optional chaining to the docs component

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 interface PageProps {
-  params: { slug: string[] };
+  params: { slug?: string[] };
 }
 
 const Docs = ({ params }: PageProps) => {
-  if (params.slug.length === 2) {
+  if (params.slug?.length === 2) {
     return (
       <h1>
         Viewing docs for feature {params.slug[0]} and concept {params.slug[1]}
       </h1>
     );
-  } else if (params.slug.length === 1) {
+  } else if (params.slug?.length === 1) {
     return <h1>Viewing docs for feature {params.slug[0]}</h1>;
   }
   return (
