5e6b58119ad33edc1e2ba142c34b674f9db9a6fb
purushothamguda <99022017+purushothamguda@users.noreply.github.com>
2025-03-12 15:18:24 +0530

    Update page.tsx

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ProductDetails = ({ params }: { params: { productId: string } }) => {
+const ProductDetails = ({ params }: { params: { productId: string; } }) => {
   return (
     <div>
       <h1>Details about Product {params.productId}</h1>
