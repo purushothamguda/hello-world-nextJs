2188874bf28f53da58ebdbce4147ba2fb1084819
Purushotham Guda <purushotham.guda49@gmail.com>
2025-03-12 13:28:57 +0530

    tutorial-11 File Colocation

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+export function BarChart() {
+  return <h1>BarChart</h1>;
+}
+
+export default function Dashboard() {
+  return (
+    <>
+      <h2>Dashboard</h2>
+      <BarChart />
+    </>
+  );
+}
