d35af61d2c3916756f6edb78ff01c2b29e0bc309
Purushotham Guda <purushotham.guda49@gmail.com>
2025-03-25 11:40:51 +0530

    Tutorial - 22 - Templates  && Tutorial - 23 - Loading UI

---
+++ src/app/(auth)/template.tsx
@@ -0,0 +1,55 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const navLinks = [
+  {
+    name: "Register",
+    href: "/register",
+  },
+  {
+    name: "Login",
+    href: "/login",
+  },
+  {
+    name: "Forgot Password",
+    href: "/forgot-password",
+  },
+];
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathName = usePathname();
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter the text"
+          onChange={(e) => {
+            e.target.value;
+          }}
+        />
+      </div>
+      <div>
+        {navLinks.map((link) => {
+          const isActive =
+            pathName === link.href ||
+            (pathName.startsWith(link.href) && link.href !== "/");
+          return (
+            <Link
+              className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+              href={link.href}
+              key={link.name}
+            >
+              {link.name}
+            </Link>
+          );
+        })}
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default AuthLayout;
